b3f751469cbc825bae3c5037600280249bfc11ed
Ashley <ash.woodiss-field@dbca.wa.gov.au>
2024-06-14 02:42:09 +0000

    Update: auto preference switch applies for non-CM WL officers set to pcm

--- wildlifecompliance/context_processors.py
+++ wildlifecompliance/context_processors.py
@@ -5,10 +5,12 @@ from wildlifecompliance.components.users.models import (ComplianceManagementUser
 
 def authorised_index(request):
 
-    print(is_wildlife_compliance_officer(request),
-        is_compliance_management_user(request))
-
     if is_wildlife_compliance_officer(request):
+        if not is_compliance_management_user and prefer_compliance_management(request):
+            #if the user is NOT a compliance management user but IS a wildlife compliance officer, change their preference
+            preferences = ComplianceManagementUserPreferences.objects.get(email_user_id=request.user.id)
+            preferences.prefer_compliance_management = False
+            preferences.save()
         return {"authorised_index":"app"}
     elif is_compliance_management_user(request):
         if not prefer_compliance_management(request):
