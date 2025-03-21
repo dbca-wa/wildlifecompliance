d19faf4d1e1ae4c850fc74338e51b56454f9669f
Jason Moore <jmoore@austwa.com>
2025-03-21 14:32:43 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-  
+
 # Start the first process
-env > /etc/.cronenv
-sed -i 's/\"/\\"/g' /etc/.cronenv
-cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/rand_hash
+cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
-service cron start &
-status=$?
-if [ $status -ne 0  ]; then
-      echo "Failed to start cron: $status"
-        exit $status
-    fi
+if [ $ENABLE_CRON == "True" ];
+then
+	echo "Starting Python Cron"
+	python /bin/scheduler.py /app/python-cron /app/logs/python-cron.log &
+	status=$?
+	if [ $status -ne 0 ]; then
+		  echo "Failed to start cron: $status"
+		    exit $status
+	fi
 
-    # Start the second process
-    gunicorn wildlifecompliance.wsgi --bind :8080 --config /app/gunicorn.ini
-    status=$?
-    if [ $status -ne 0  ]; then
-          echo "Failed to start gunicorn: $status"
-            exit $status
-        fi
+fi
+
+if [ $ENABLE_WEB == "True" ];
+	    then
+		    echo "Starting Gunicorn"
+		    # Start the second process
+
+		    /app/venv/bin/gunicorn wildlifecompliance.wsgi --bind :8080 --config /app/gunicorn.ini
+		    status=$?
+		    if [ $status -ne 0 ]; then
+			      echo "Failed to start gunicorn: $status"
+			        exit $status
+		    fi
+	    else
+		       echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+		          /bin/bash
+fi
