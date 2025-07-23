c8377ce328ee510b528cc52db9cd24c4824c39b1
Ash <ash.woodiss-field@dbca.wa.gov.au>
2025-07-23 09:48:12 +0800

    Added: fetch func

--- wildlifecompliance/frontend/wildlifecompliance/src/utils/hooks.js
+++ wildlifecompliance/frontend/wildlifecompliance/src/utils/hooks.js
@@ -1,8 +1,10 @@
 import api_endpoints from '@/api.js';
 import helpers from './helpers';
 import cache_helper from './cache_helper';
+import fetch from './fetch';
 export {
   api_endpoints,
   helpers,
   cache_helper,
+  fetch,
 }
